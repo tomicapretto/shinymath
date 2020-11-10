79c6b328acc5310b863daddb2712bc58ece457d7
tomicapretto <tomicapretto@gmail.com>
2020-11-10 02:19:31 -0300

    Add documentation. Reorder functions. Update README

--- R/updateMathInput.R
+++ R/updateMathInput.R
@@ -1,5 +1,16 @@
+#' Chabnge the value of a mathematical input on the client
+#'
+#' @param session The `session` object passed to function given to `shinyServer`.
+#' @param inputId The id of the input object.
+#' @param label The label to set for the input object.
+#' @param value The value to set for the input object.
+#'
+#' @export
 updateMathInput = function(session = shiny::getDefaultReactiveDomain(),
                            inputId, label = NULL, value = NULL) {
-  message = shiny:::dropNulls(list(label = label, value = value))
+  message = drop_nulls(list(label = label, value = value))
   session$sendInputMessage(inputId, message)
 }
+
+
+
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,7 @@
 shinymath_file = function(...) {
   system.file(..., package = "shinymath", mustWork = TRUE)
 }
+
+drop_nulls = function(x) {
+  x[!vapply(x, is.null, FUN.VALUE = logical(1))]
+}
